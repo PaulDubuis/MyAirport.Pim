deda736a80c8a4cd6652e06dff707cf9d0fa0b7a
Guillaume Malle <guillaumemalle2@gmail.com>
2018-12-10 18:53:00 +0100

    Added Models.sql (not implemented yet) And dbconnect property in client.formlhm

---
+++ MyAirport.Pim/Models.Sql/Sql.cs
@@ -0,0 +1,23 @@
+﻿using MyAirport.Pim.Entities;
+using MyAirport.Pim.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Sql
+{
+    public class Sql : AbstractDefinition
+    {
+        public override BagageDefinition GetBagage(int idBagage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<BagageDefinition> GetBagage(string codeIataBagage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
